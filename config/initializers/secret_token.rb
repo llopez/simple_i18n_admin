ae67532c5a343b1247c66f2af8c0f11b6fde80f9
Luis Lopez <luigibyte@gmail.com>
2011-04-12 17:42:18 -0300

    the first commit, Hello World.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleI18nAdmin::Application.config.secret_token = '18e468b8f165c4eedc07455f9d1605680e433211a1aca72880ff1cc51c0566d15d4998d37b84f1b3cbdbf3b391a062b79563513e2c5b277fd7c87583cbc2a04a'
